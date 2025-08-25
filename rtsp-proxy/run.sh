088ca28a09204826c29d4d87152d3c1a7a3b23c6
nicoolaj <529254+nicoolaj@users.noreply.github.com>
2025-08-25 18:18:00 +0200

    Update run.sh

--- rtsp-proxy/run.sh
+++ rtsp-proxy/run.sh
@@ -35,5 +35,5 @@ bashio::log.info "Configuration générée. Démarrage du serveur mediamtx..."
 cat ${CONFIG_FILE}
 
 # Lancer le serveur mediamtx avec notre fichier de configuration
-exec /usr/local/bin/mediamtx ${CONFIG_FILE}
+exec /usr/local/bin/mediamtx -conf ${CONFIG_FILE}
 
